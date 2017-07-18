e75f852f267c0ab1fde0a9cfc5f32b0bbe849c0a
Sven Fischer <sven.fischer85@gmx.de>
2017-07-19 01:33:37 +0200

    Import package jwt via wiki (works, but no aot)

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as jwt from 'angular2-jwt/angular2-jwt';
 
 /**
  * This class represents the lazy loaded AboutComponent.
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
   templateUrl: 'about.component.html',
   styleUrls: ['about.component.css']
 })
-export class AboutComponent { }
+export class AboutComponent {
+    constructor() {
+        console.log(jwt.AuthConfig);
+    }
+}
+
